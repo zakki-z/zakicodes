48117a0926b9362da884903258a084391457ef87
zakki-z <159003869+zakki-z@users.noreply.github.com>
2025-08-30 15:21:58 +0100

    added vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { HeroSection } from '@/components/sections/hero';
-
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
     <main className="w-full">
         <HeroSection />
-
+        <Analytics />
     </main>
   );
 }
